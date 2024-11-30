245b4466a82c509a27f585a29a00356eb4d38683
jeetenmndl <jeetenmndl96@gmail.com>
2024-11-30 16:11:26 +0545

    check

---
+++ src/lib/actions/getAllIssues.js
@@ -0,0 +1,30 @@
+"use server"
+
+import { currentUser } from "@clerk/nextjs/server";
+
+
+const getAllIssues = async ()=>{
+
+    const user = await currentUser();
+
+    let details = {
+        userID: user.id,
+    }
+    // console.log(details)
+
+  
+    const settings = {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(details)
+  };
+  
+    const query = await fetch(`${process.env.SERVER}/get_all_issues_api`, settings)
+    const response = await query.json()
+  
+    return response
+  }
+
+  export default getAllIssues;
